5e862f3b1c31e9fd973f5aa752ff9283756a5267
Leo Bailey <leobailey@hotmail.com>
2019-04-30 15:14:52 +0100

    title change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!!! - node version");
+    response.end("Hello World!!! - node version (but soon to be express!)");
 });
 
 const port = process.env.PORT || 1337;
