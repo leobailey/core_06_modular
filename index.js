fbae32ed4fe29485893f1522d3d30816e1cad68c
Leo Bailey <leobailey@hotmail.com>
2019-04-30 17:03:50 +0100

    express route

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const http = require('http');
+//const http = require('http');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 1337;
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!!! - node version (but soon to be express!)");
+
+app.get('/',(req, res)=>{
+    res.send('Welcome to my API');
 });
 
-const port = process.env.PORT || 1337;
-server.listen(port);
+/* const server = http.createServer((request, response) => {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!!! - node version (but soon to be express!)");
+}); */
 
-console.log("Server running at http://localhost:%d", port);
+
+server.listen(port,()=>{
+    console.log("Server running at http://localhost:%d", port);
+});
