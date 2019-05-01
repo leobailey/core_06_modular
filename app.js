5219f4079599f37f2201f820029bfce4d07a7806
Leo Bailey <leobailey@hotmail.com>
2019-05-01 16:55:49 +0100

    test the all environments work now index has been transitioned to app

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 
 app.get('/', (req, res)=>{
-    res.send('Welcome to my API - just express at the moment');
+    res.send('Welcome to my API - just express at the moment !');
 });
 
 /* const server = http.createServer((request, response) => {
