4f5f2b4bbc147a2651bb27cf2a6bc784a75b25db
Leo Bailey <leobailey@hotmail.com>
2019-05-07 17:38:07 +0100

    starting routing

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 
+const demoRouter = express.Router();
+
+demoRouter.route('/books')
+    .get((req, res) =>{
+        const response = { hello: 'this is my API' };
+        res.json(response);
+    });
+
+app.use('/api', demoRouter);
 
 app.get('/', (req, res)=>{
     res.send('Welcome to my API - just express at the moment with some nodemon !!');
 });
 
-/* const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!!! - node version (but soon to be express!)");
-}); */
-
 app.listen(port,()=>{
     console.log("Server running at http://localhost:%d", port);
 });
+
