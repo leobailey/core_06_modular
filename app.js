6c0048e3a40c1a65235a7dd342fdbae62c846427
Leo Bailey <leobailey@hotmail.com>
2019-05-13 14:42:47 +0100

    implement mongoose + schema + model + cloud database connection

--- app.js
+++ app.js
@@ -7,23 +7,30 @@ const path = require('path');
 
 const demoRouter = express.Router();
 const db = mongoose.connect('mongodb://coredbUser:c0r3dbuser@ds044787.mlab.com:44787/coredb?authSource=coredb')
-// const Book = require('./models/bookModel');
+const Book = require('./models/bookModel');
 
-// viewed at http://localhost:8080
+// servers the html page
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+// servers the demorouter demo api endpoint
 demoRouter.route('/Demo')
 .get((req, res)=> {
-    const response = {hello: 'This is my DEMO API'};
+    const response = {hello: 'This is my Demo API'};
     res.json(response);
+
 });
 
+// servers the demo router books api endpoint
 demoRouter.route('/Books')
 .get((req, res)=> {
-    const response = {hello: 'This is my Books API'};
-    res.json(response);
+    Book.find((err, books) => {
+        if (err){
+            return res.send(err);
+        } 
+        return res.json(books);
+    });
 });
 
 /* demoRouter.route('/books')
@@ -36,11 +43,13 @@ demoRouter.route('/Books')
         });
     }); */
 
+// defines the entry point for the demoRouter Router    
 app.use('/api', demoRouter);
 
-app.get('/', (req, res)=>{
+// This has been deprecated by the html page served above
+/* app.get('/', (req, res)=>{
     res.send('Welcome to my API - just express at the moment with some nodemon !!');
-});
+}); */
 
 app.listen(port,()=>{
     console.log("Server running at http://localhost:%d", port);
--- models/bookModel.js
+++ models/bookModel.js
@@ -2,12 +2,11 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 const bookModel = new Schema(
     {
-        _id: {type:ObjectId},
-        ID: {type:Int32},
+        Id: {type:Number},
         Date: {type:String},
         ApiMethod: {type:String},
         IPAddress:{type:String},
-        EmailAddress:{type:String}
+        EmailAddress:{type:String},
     }
 );
 
