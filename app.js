673a01ca74ef7926a2a7e759aafb9114173b7555
Leo Bailey <leobailey@hotmail.com>
2019-06-07 12:53:54 +0100

    implemented basic IPlookup

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//const http = require('http');
+const http = require('http');
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
@@ -37,6 +37,22 @@ demoRouter.route('/Books')
     });
 });
 
+
+// Basic implementation of the IPAddress Lookup - takes a static IP and sends it to the service and gets the details back
+demoRouter.route('/iplookup')
+.get((req, res)=> {
+    var Request = require("request");
+    Request.get("http://api.ipstack.com/193.23.120.234?access_key=6f2e653b548e543715e59299f35fc40d", (error, response, body) => {
+        if(error) {
+            res.send(error);
+            return console.dir(error);
+            
+        }
+        res.send(JSON.parse(body));
+        return console.dir(JSON.parse(body));
+    })
+});
+
 // servers the demo router book (look fo id) api endpoint
 demoRouter.route('/Books/:bookId')
 .get((req, res)=> {
@@ -48,23 +64,8 @@ demoRouter.route('/Books/:bookId')
     });
 });
 
-/* demoRouter.route('/books')
-    .get((req, res) =>{
-        Book.find((err, books) => {
-            if(err){
-                return res.send(err);
-            }
-            return res.json(books);
-        });
-    }); */
-
 // defines the entry point for the demoRouter Router    
 app.use('/api', demoRouter);
-
-// This has been deprecated by the html page served above
-/* app.get('/', (req, res)=>{
-    res.send('Welcome to my API - just express at the moment with some nodemon !!');
-}); */
 
 app.listen(port,()=>{
     console.log("Server running at http://localhost:%d", port);
